8d3db54ad0cb66666a40bbefebdca6564ee95fc9
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2022-11-24 12:25:32 +0600

    some package and pages and folder structure added

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
+import useToken from "../../Hooks/useToken";
+
+
+const Login = () => {
+
+  return (
+    <div className="">
+      
+    </div>
+  );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import 'react-day-picker/dist/style.css';
+import AuthProvider from './Context/AuthProvider/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
         <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
