e1c1b1447b89486bed8ba43ab4bec15bdbf50498
1Md-Rakibul-Islam <rakibul.islam199tech@gmail.com>
2023-03-18 21:51:27 +0600

    Profile card added on dshoboard profile

--- src/Pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { FaBoxOpen, FaStoreAlt, FaUsers } from "react-icons/fa";
+import UserProfile from "../../../Components/UserProfile";
 import { AuthContext } from "../../../Context/AuthProvider/AuthProvider";
 import useAdmin from "../../../Hooks/useAdmin";
 import useSeller from "../../../Hooks/useBooking";
@@ -19,7 +20,7 @@ const Dashboard = () => {
 
 
   return (
-      <section className="p-6 my-6 bg-gray-200 text-gray-100">
+      <section className="p-6 my-6 h-full bg-gray-200 text-gray-100">
         <div className="container grid grid-cols-1 gap-6 mx-auto sm:grid-cols-2 xl:grid-cols-3">
           <div className="flex p-8 space-x-4 rounded-lg md:space-x-6 bg-gray-900 text-gray-100">
             <div className="flex justify-center p-2 align-middle rounded-lg sm:p-4 bg-primary">
@@ -53,6 +54,97 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
+        
+        <div  className="grid grid-cols-1 sm:grid-cols-2 gap-5 justify-center sm:justify-between items-start mt-16" >
+          <div class="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+              <div class="flex items-center justify-between mb-4">
+                  <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
+                  <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+                      View all
+                  </a>
+            </div>
+            <div>
+                <div class="flow-root">
+                  <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+                      <li class="py-3 sm:py-4">
+                          <div class="flex items-center space-x-4">
+                              <div class="flex-shrink-0">
+                                  <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image" />
+                              </div>
+                              <div class="flex-1 min-w-0">
+                                  <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                      Neil Sims
+                                  </p>
+                                  <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                      email@windster.com
+                                  </p>
+                              </div>
+                              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                  $320
+                              </div>
+                          </div>
+                      </li>
+                      <li class="py-3 sm:py-4">
+                          <div class="flex items-center space-x-4">
+                              <div class="flex-shrink-0">
+                                  <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image" />
+                              </div>
+                              <div class="flex-1 min-w-0">
+                                  <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                      Bonnie Green
+                                  </p>
+                                  <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                      email@windster.com
+                                  </p>
+                              </div>
+                              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                  $3467
+                              </div>
+                          </div>
+                      </li>
+                      <li class="py-3 sm:py-4">
+                          <div class="flex items-center space-x-4">
+                              <div class="flex-shrink-0">
+                                  <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Michael image" />
+                              </div>
+                              <div class="flex-1 min-w-0">
+                                  <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                      Michael Gough
+                                  </p>
+                                  <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                      email@windster.com
+                                  </p>
+                              </div>
+                              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                  $67
+                              </div>
+                          </div>
+                      </li>
+                      <li class="py-3 sm:py-4">
+                          <div class="flex items-center space-x-4">
+                              <div class="flex-shrink-0">
+                                  <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-4.jpg" alt="Lana image" />
+                              </div>
+                              <div class="flex-1 min-w-0">
+                                  <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                      Lana Byrd
+                                  </p>
+                                  <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                      email@windster.com
+                                  </p>
+                              </div>
+                              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                  $367
+                              </div>
+                          </div>
+                      </li>
+                  </ul>
+            </div>
+            </div>
+          </div>
+          <UserProfile />
+        </div>
+
       </section>
     // <div className="mt-10 flex md:flex-nowrap flex-wrap justify-evenly gap-5 bg-base-200 shadow-xl py-10 rounded-lg items-center">
     //   <div className="">
