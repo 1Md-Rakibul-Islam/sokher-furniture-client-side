28cdce71f410b7e00da61fb78abd24df0d65f8d1
1Md-Rakibul-Islam <rakibul.islam199tech@gmail.com>
2023-03-25 23:48:30 +0600

    cliens section marquee addeed

--- src/Pages/Home/Clients/Clients.jsx
+++ src/Pages/Home/Clients/Clients.jsx
@@ -10,13 +10,13 @@ const Clients = () => {
     const clients = [slider1, slider2, slider3, slider4, slider5, slider6];
 
     return (
-        <section className='py-16 my-5 border border-y-2 border-x-0'>
+        <marquee direction="right" behavior="scroll" scrollamount="20"  className='py-16 my-5 border border-y-2 border-x-0'>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 justify-between items-center'>
                 {
                     clients?.map(client => <div className='mx-auto'><img className='w-28' src={client} /></div>)
                 }
             </div>
-        </section>
+        </marquee>
     );
 };
 
