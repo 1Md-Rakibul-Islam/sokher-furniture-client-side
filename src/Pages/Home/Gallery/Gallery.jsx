6edfa65e554835b50bb496949ed5df87a1c03960
1Md-Rakibul-Islam <rakibul.islam199tech@gmail.com>
2023-03-25 23:40:35 +0600

    gallery responsive

--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -4,49 +4,38 @@ const Gallery = () => {
 
     return (
         <section className='my-20'>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-                <div className="grid gap-4">
+            <div className="flex md:gap-5 sm:gap-3 gap-1">
+                <div className="flex flex-col md:gap-5 sm:gap-3 gap-1">
                     <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/XWftZ5q/h1.jpg" alt="" />
+                        <img className="h-full w-full rounded-lg hover:shadow-2xl hover:shadow-purple-500" src="https://i.ibb.co/XWftZ5q/h1.jpg" alt="" />
                     </div>
                     <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/zstC8D9/s1.jpg" alt="" />
+                        <img className="h-36 md:h-60 w-full rounded-lg hover:shadow-2xl hover:shadow-purple-500" src="https://i.ibb.co/zstC8D9/s1.jpg" alt="" />
                     </div>
                     <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/nCN9tk5/s5.jpg" alt="" />
-                    </div>
-                </div>
-                <div className="grid gap-4">
-                    <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/zGxBwqt/s3.jpg" alt="" />
-                    </div>
-                    <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/Pt2LsfD/h2.jpg" alt="" />
-                    </div>
-                    <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/wLw8DTT/w2.jpg" alt="" />
+                        <img className="h-full w-full rounded-lg hover:shadow-2xl hover:shadow-purple-500" src="https://i.ibb.co/nCN9tk5/s5.jpg" alt="" />
                     </div>
                 </div>
-                <div className="grid gap-4">
+                <div className="flex flex-col md:gap-5 sm:gap-3 gap-1">
                     <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/S3PsmNm/s4.jpg" alt="" />
+                        <img className="h-full w-full rounded-lg hover:shadow-2xl hover:shadow-purple-500" src="https://i.ibb.co/zGxBwqt/s3.jpg" alt="" />
                     </div>
                     <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/sjjWhYh/w3.jpg" alt="" />
+                        <img className="sm:h-full h-56 w-full rounded-lg hover:shadow-2xl hover:shadow-purple-500" src="https://i.ibb.co/Pt2LsfD/h2.jpg" alt="" />
                     </div>
                     <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/02Xttwn/w1.jpg" alt="" />
+                        <img className="h-full w-full rounded-lg hover:shadow-2xl hover:shadow-purple-500" src="https://i.ibb.co/wLw8DTT/w2.jpg" alt="" />
                     </div>
                 </div>
-                <div className="grid gap-4">
+                <div className="flex flex-col md:gap-5 sm:gap-3 gap-1">
                     <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/1d10khP/s2.jpg" alt="" />
+                        <img className="h-full w-full rounded-lg hover:shadow-2xl hover:shadow-purple-500" src="https://i.ibb.co/S3PsmNm/s4.jpg" alt="" />
                     </div>
                     <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/Pt2LsfD/h2.jpg" alt="" />
+                        <img className="md:h-60 h-36 w-full rounded-lg hover:shadow-2xl hover:shadow-purple-500" src="https://i.ibb.co/sjjWhYh/w3.jpg" alt="" />
                     </div>
                     <div>
-                        <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/02Xttwn/w1.jpg" alt="" />
+                        <img className="h-full w-full rounded-lg hover:shadow-2xl hover:shadow-purple-500" src="https://i.ibb.co/1d10khP/s2.jpg" alt="" />
                     </div>
                 </div>
             </div>
--- src/Pages/Home/Home/ContactUs/ContactUs.jsx
+++ src/Pages/Home/Home/ContactUs/ContactUs.jsx
@@ -32,16 +32,16 @@ const ContactUs = () => {
                 <form novalidate="" className="flex  flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid">
                 
                 <div>
-                    <label for="last_name" class="block mb-2 text-lg font-medium text-gray-900 ">Last name</label>
-                    <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Johan Khan" required />
+                    <label for="last_name" className="block mb-2 text-lg font-medium text-gray-900 ">Last name</label>
+                    <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Johan Khan" required />
                 </div>
-                <div class="mb-6">
-                    <label for="email" class="block mb-2 text-lg font-medium text-gray-900 ">Email address</label>
-                    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="john.doe@company.com" required />
+                <div className="mb-6">
+                    <label for="email" className="block mb-2 text-lg font-medium text-gray-900 ">Email address</label>
+                    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="john.doe@company.com" required />
                 </div>
                 
-                <label for="message" class="block mb-2 text-lg font-medium text-gray-900 ">Your message</label>
-                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+                <label for="message" className="block mb-2 text-lg font-medium text-gray-900 ">Your message</label>
+                <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
  
                     <button type="button" className="btn btn-primary rounded-full">Submit</button>
                 </form>
